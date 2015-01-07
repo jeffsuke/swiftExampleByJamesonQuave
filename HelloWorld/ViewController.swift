0449403219c300c6274da4d4d6abd3c4e363e8e2
Yusuke Kawanabe <ysk.kwnb.915@gmail.com>
2015-01-07 18:36:04 +0900

    Add search itunes for method

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    @IBOutlet weak var appsTableView: UITableView?
-     
+    @IBOutlet weak var appsTableView: UITableView!
+    var tableData = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         println("Hello World")
@@ -32,6 +33,33 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
+    
+    func searchItunesFor(searchTerm: String) {
+        let itunesSearchTearm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        if let escapedSearchTerm = itunesSearchTearm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+            let url = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                println("Task completed")
+                if (error != nil) {
+                    println(error.localizedDescription)
+                }
+                var err: NSError?
+                
+                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+                println("JSON Error \(err?.localizedDescription)")
+                
+                let results = jsonResult["results"] as NSArray
+                dispatch_async(dispatch_get_main_queue(), {
+                self.tableData = results
+                self.appsTableView!.reloadData()
+                })
+            })
 
+        }
+    
+    }
 }
 
